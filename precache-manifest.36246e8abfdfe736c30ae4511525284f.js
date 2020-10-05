114ea4d8605036c3c45250942704243702180733
Robin-shrestha <rsresthas@gmail.com>
2020-10-05 15:42:57 +0545

    Updates

---
+++ precache-manifest.36246e8abfdfe736c30ae4511525284f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55adf3c521e8149df8f60baf196c848f",
+    "url": "/DirectTechNepal/index.html"
+  },
+  {
+    "revision": "86d3de7af58fe57c34f2",
+    "url": "/DirectTechNepal/static/css/2.445b666f.chunk.css"
+  },
+  {
+    "revision": "7591d68f66c77fdd3226",
+    "url": "/DirectTechNepal/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "86d3de7af58fe57c34f2",
+    "url": "/DirectTechNepal/static/js/2.82aa982b.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/DirectTechNepal/static/js/2.82aa982b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7591d68f66c77fdd3226",
+    "url": "/DirectTechNepal/static/js/main.679fc5d4.chunk.js"
+  },
+  {
+    "revision": "aebc197befdcb8188f35",
+    "url": "/DirectTechNepal/static/js/runtime-main.9875ecd6.js"
+  },
+  {
+    "revision": "db9d42a8adcde559aeb6786129298439",
+    "url": "/DirectTechNepal/static/media/1.db9d42a8.jpeg"
+  },
+  {
+    "revision": "b6793510d0ef108c7ee185213f0d2ea6",
+    "url": "/DirectTechNepal/static/media/2.b6793510.jpg"
+  },
+  {
+    "revision": "945ba71e3f96a7d07bc3ff28359a3cec",
+    "url": "/DirectTechNepal/static/media/3.945ba71e.jpg"
+  },
+  {
+    "revision": "4ae03807d31260c46faa28cb7643b8d8",
+    "url": "/DirectTechNepal/static/media/logoDesign.4ae03807.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/DirectTechNepal/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/DirectTechNepal/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/DirectTechNepal/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/DirectTechNepal/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "9e7cf8977d788ace61b4ac0b3be68e01",
+    "url": "/DirectTechNepal/static/media/webdev.9e7cf897.png"
+  }
+]);
