7eccc007218ef95e6b6952aa124b6cc12b7aaec1
Robin-shrestha <rsresthas@gmail.com>
2020-10-11 10:53:24 +0545

    first commit on spring branch

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import { Switch, Route, Redirect } from "react-router-dom";
-// import Footer from "./components/footer/Footer";
+import Footer from "./components/footer/Footer";
 function App() {
   return (
     <div style={{ backgroundColor: "#fff" }}>
@@ -13,7 +13,7 @@ function App() {
           <Redirect to="/" />
         </Route>
       </Switch>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
